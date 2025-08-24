15c9d2d36cdbb79a7c86502f5aa6ec4535135235
Angelo Rafael Recio <recioangelorafael@gmail.com>
2025-08-24 15:06:02 +0800

    improved habit-tracker interfaces

--- src/interfaces/Habits.ts
+++ src/interfaces/Habits.ts
@@ -1,16 +1,19 @@
-import { Schema } from 'mongoose';
+import { Schema, Document, } from 'mongoose';
 
-export interface IUser {
+export interface IUser extends Document<any, any, IUser> {
   discordId: string;
   username: string;
+  dateJoined: Date;
   habits: Schema.Types.ObjectId[]; // This will store an array of ObjectIDs from the Habit model
 }
 
-export interface IHabit {
+export interface IHabit extends Document<any, any, IHabit> {
   name: string;
   description?: string;
   ownerId: Schema.Types.ObjectId; // This will reference the User model's ID
   streak: number;
+  lastAdded: Date;
+  lastUpdate: Date;
   lastCompleted: Date;
   history: Date[]; // An array to store the dates the habit was completed
 }
